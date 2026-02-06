e14589399c33869a7f34602000516aba3c4d36b7
Sirayuth <h.sirayuth13@gmail.com>
2026-02-06 16:17:58 +0700

    Update ClassDesign

---
+++ ClassDesign/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDesign;
+
+public class Product
+{
+    public required string Name { get; init; }
+    public required int Quantity { get; init; }
+    public required decimal Price { get; init; }
+
+    // Todo#1: create computed property "TotalPrice" as decimal
+    // Todo#2: override ToString() method
+}
